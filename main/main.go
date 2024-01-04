a9788d9f01e566340852ee2b093dacbe5922c680
jeeseungbae <sdgh1442@gmail.com>
2024-01-05 05:12:42 +0900

    bit making add 시간초과 + test3 Error

--- main/main.go
+++ main/main.go
@@ -22,53 +22,82 @@ func main() {
 
 func BuildBlocks(w int, h int) int {
 	answer := 1
-	basicBlock := []int{1, 2, 3}
-	block := make([]bool, w)
+	basicBlock := []int64{1, 3, 5}
+
+	ground := int64(0)
 	for i := 0; i < w; i++ {
-		block[i] = true
+		ground = ground << 1
+		ground += 1
 	}
 
-	checkBlock(block, make([]bool, w), basicBlock, 0, w, h, &answer)
+	checkBlock(ground, 0, basicBlock, 0, w, h, &answer)
 
 	return answer
 }
 
-func checkBlock(block []bool, upBlock []bool, basicBlock []int, start int, w int, h int, answer *int) {
+func checkBlock(ground int64, block int64, basicBlock []int64, start int, w int, h int, answer *int) {
 	if h == 0 {
 		return
 	}
 	if start >= w {
 		return
 	}
+	if block < 0 {
+		return
+	}
+
 	for i := 0; i < len(basicBlock); i++ {
-		if block[start] && start+basicBlock[i]-1 < w && block[start+basicBlock[i]-1] {
-			*answer += 1
-			blockBuild(upBlock, true, start, start+basicBlock[i])
+		if w >= start+i+1 {
+			data := block<<i + 1
+			data = data | basicBlock[i]
 
-			stacking(upBlock, basicBlock, w, h, answer)
-
-			moving(block, upBlock, basicBlock, start+basicBlock[i], w, h, answer)
-
-			blockBuild(upBlock, false, start, start+basicBlock[i])
+			if IsContain(ground, data) {
+				*answer += 1
+				stacking(data, basicBlock, w, h-1, answer)
+				moving(ground, data, basicBlock, start+i+1, w, h, answer)
+			}
 		}
 	}
-	// 이동
-	moving(block, upBlock, basicBlock, start+1, w, h, answer)
+	moving(ground, block<<1, basicBlock, start+1, w, h, answer)
 }
 
-func stacking(upBlock []bool, basicBlock []int, w int, h int, answer *int) {
-	// 위로 쌓기
-	checkBlock(upBlock, make([]bool, w), basicBlock, 0, w, h-1, answer)
-}
+func IsContain(ground int64, data int64) bool {
+	groundBlock := fmt.Sprintf("%010b", ground)
+	dataBlock := fmt.Sprintf("%010b", data)
 
-func moving(block []bool, upBlock []bool, basicBlock []int, start int, w int, h int, answer *int) {
-	// 쌓고 이동
-	checkBlock(block, upBlock, basicBlock, start, w, h, answer)
-}
-
-func blockBuild(upBlock []bool, flag bool, start int, end int) []bool {
-	for i := start; i < end; i++ {
-		upBlock[i] = flag
+	for i := 0; i < len(dataBlock); i++ {
+		if dataBlock[i] == '1' {
+			ln, rn := blockJump(dataBlock, i)
+			if enableBlock(groundBlock, dataBlock, ln, rn-1) {
+				i += rn - 1 - ln
+			} else {
+				return false
+			}
+		}
 	}
-	return upBlock
+	return true
+}
+
+func blockJump(dataBlock string, index int) (int, int) {
+	for i := index + 1; i < len(dataBlock); i++ {
+		if dataBlock[i] != '1' {
+			return index, i
+		}
+	}
+	return index, len(dataBlock)
+}
+
+func enableBlock(groundBlock string, dataBlock string, start int, end int) bool {
+	if groundBlock[start] == dataBlock[start] && groundBlock[end] == dataBlock[end] {
+		return true
+	}
+	return false
+}
+
+func stacking(data int64, basicBlock []int64, w int, h int, answer *int) {
+	checkBlock(data, 0, basicBlock, 0, w, h, answer)
+}
+
+func moving(ground int64, data int64, basicBlock []int64, start int, w int, h int, answer *int) {
+	checkBlock(ground, data, basicBlock, start, w, h, answer)
 }
